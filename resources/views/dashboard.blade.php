5e383a3f8ecb61f728bfd71e64d63d6682e5d42a
Andres Hernandez <andr3sm1guel@gmail.com>
2021-09-19 21:52:43 -0300

    table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,58 +7,51 @@
 
     <div class="container-fluid card p-2">
 
-    <div class="card m-2">
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>24h%</th>
+            <th>7d%</th>
+            <th>Cap. de mercado</th>
+            <th>Volume ( 24h )</th>
+            <th>Ações em circulaçã </th>
+        </tr>
+
+            @foreach($data as $key)
+                <tr>
+                    <td>{{ $key['name'] }} {{ $key['symbol'] }}</td>
+                    @foreach($key['quote'] as $key2 )
+                    <td> $ {{ number_format($key2['price'], 2) }}</td>
+                    <td>{{ number_format($key2['percent_change_24h'], 2) }}</td>
+                    <td>{{ number_format($key2['percent_change_7d'], 2) }}</td>
+                    <td> $ {{ number_format($key2['market_cap'], 0) }}</td>
+                    <td> $ {{ number_format($key2['volume_24h'], 0) }}</td>
+
+                    @endforeach
+                    <td>{{ number_format($key['circulating_supply'], 0) }} {{ $key['symbol'] }}</td>
+                </tr>
+            @endforeach
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+
+    </table>
+
+   {{--  <div class="card m-2">
         <div class="card-body">
+            <span class="m-4"></span>
+            <span class="m-4">Preço</span>
+            <span class="m-4"></span>
+            <span class="m-4"></span>
+            <span class="m-4"></span>
+            <span class="m-4"</span>
+            <span class="m-4">o</span>
+
         </div>
     </div>
 
-    @foreach($data as $key)
-        <div class="card border m-2">
-            <div class="card-body">
-                {{ $key['name'] }}
-                @foreach($key['quote'] as $key2 )
-                    {{ number_format($key2['price'], 2) }}
-                @endforeach
-            </div>
-        </div>  
-    @endforeach
+         --}}
 
-    
-        
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">Preço</th>
-                <th scope="col">24h%</th>
-                <th scope="col">7d%</th>
-                <th scope="col">Cap. de mercado</th>
-                <th scope="col">Volume 24h</th>
-                <th scope="col">Ações em circulação</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($data as $key)
-                <tr>
-                    <td> {{ $key['name'] }} </td>
-                    @foreach($key['quote'] as $key2 )
-                        <td>$ {{ number_format($key2['price'], 2) }} </td>
-                    @endforeach
-                </tr>
-            @endforeach
-
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Nombre</td>
-                    <td>Precio</td>
-                    <td>24h%</td>
-                    <td>7d%</td>
-                    <td>Cap.de Mercado</td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-        
     </div>
 </x-app-layout>
